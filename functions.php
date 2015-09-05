f3a56a13b7761fc0541cc47c9533949ca27ba4d1
Larry Kagan <larry@superiocity.com>
2015-09-05 02:10:03 -0400

    Allow featured image on posts.

--- functions.php
+++ functions.php
@@ -1,5 +1,5 @@
 <?php
-	
+
 // Add RSS links to <head> section
 add_theme_support( 'automatic-feed-link' );
 
@@ -7,11 +7,17 @@ add_theme_support( 'automatic-feed-link' );
 function enqueue_superiocity_scripts() {
 	if ( ! is_admin() ) {
 		wp_deregister_script( 'jquery' );
-		wp_register_script( 'jquery', ( '//ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js' ), false );
-		wp_register_script( 'superiocity-main', get_bloginfo( 'template_url' ) . '/javascript/main.min.js', false, '20150113', false );
-		wp_register_style( 'font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css',
+		wp_register_script( 'jquery',
+			( '//ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js' ),
 			false );
-		wp_register_style( 'google-fonts', '//fonts.googleapis.com/css?family=Muli', false );
+		wp_register_script( 'superiocity-main',
+			get_bloginfo( 'template_url' ) . '/javascript/main.min.js', false,
+			'20150113', false );
+		wp_register_style( 'font-awesome',
+			'//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css',
+			false );
+		wp_register_style( 'google-fonts',
+			'//fonts.googleapis.com/css?family=Muli', false );
 		wp_enqueue_script( 'jquery' );
 		wp_enqueue_script( 'superiocity-main' );
 		wp_enqueue_style( 'font-awesome' );
@@ -26,20 +32,22 @@ function remove_head_links() {
 	remove_action( 'wp_head', 'rsd_link' );
 	remove_action( 'wp_head', 'wlwmanifest_link' );
 }
+
 add_action( 'init', 'remove_head_links' );
 remove_action( 'wp_head', 'wp_generator' );
 
 // Declare sidebar widget zone
 if ( function_exists( 'register_sidebar' ) ) {
-	register_sidebar(array(
-		'name' => 'Sidebar Widgets',
-		'id'   => 'sidebar-widgets',
+	register_sidebar( array (
+		'name'          => 'Sidebar Widgets',
+		'id'            => 'sidebar-widgets',
 		'description'   => 'These are widgets for the sidebar.',
 		'before_widget' => '<div id="%1$s" class="widget %2$s">',
 		'after_widget'  => '</div>',
 		'before_title'  => '<h2>',
 		'after_title'   => '</h2>',
-	));
+	) );
 }
 
 add_theme_support( 'menus' );
+add_theme_support( 'post-thumbnails', array( 'post' ) );
