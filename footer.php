02a8f6e5951d47470ee1e7871c4ff6b92cd85128
Larry Kagan <larry@superiocity.com>
2015-09-12 08:46:32 -0400

    Only show media link on superiocity.com.

--- footer.php
+++ footer.php
@@ -1,6 +1,9 @@
             </div> <!-- .main -->
             <footer>
-            &copy; Superiocity, Inc. | <a href="/superiocity-media/" title="Superiocity Media">Superiocity Media</a>
+            &copy; Superiocity, Inc.
+	            <?php if( strpos($_SERVER['HTTP_HOST'], 'superiocity.com') !== false ): ?>
+	            | <a href="/superiocity-media/" title="Superiocity Media">Superiocity Media</a>
+	            <?php endif; ?>
             </footer>       
         </div>  <!-- .contiguous -->
     </div> <!-- .page -->
