f12df3735e32fe1702f3a5c1cd8ccfee0cac4766
Larry Kagan <larry@superiocity.com>
2015-09-13 08:39:46 -0400

    Added sitemap link on footer.

--- footer.php
+++ footer.php
@@ -2,7 +2,8 @@
             <footer>
             &copy; Superiocity, Inc.
 	            <?php if ( SUPERIOCITY_IS_MAIN_SITE ) : ?>
-	            | <a href="/superiocity-media/" title="Superiocity Media">Superiocity Media</a>
+	            | <a href="/superiocity-media/" title="Superiocity Media">media</a>
+	            | <a href="/sitemap/" title="sitemap">sitemap</a>
 	            <?php endif; ?>
             </footer>       
         </div>  <!-- .contiguous -->
