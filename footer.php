051d6269934ea0a54e013dd1ede6e8fedffd3ecf
Larry Kagan <larry@superiocity.com>
2015-09-20 12:06:31 -0400

    Minor footer updates.

--- footer.php
+++ footer.php
@@ -1,9 +1,13 @@
             </div> <!-- .main -->
             <footer>
-            &copy; 2007 - <?php echo date('Y'); ?> Superiocity, Inc.
+	            <div class="copyright">
+					&copy; 2007-<?php echo date('Y'); ?> Superiocity, Inc.
+				</div>
 	            <?php if ( SUPERIOCITY_IS_MAIN_SITE ) : ?>
-	            | <a href="/superiocity-media/" title="Superiocity Media">media</a>
-	            | <a href="/sitemap/" title="sitemap">sitemap</a>
+				<div class="nav">
+					<a href="/superiocity-media/" title="Superiocity Media">media</a>
+					| <a href="/sitemap/" title="sitemap">sitemap</a>
+				</div>
 	            <?php endif; ?>
             </footer>       
         </div>  <!-- .contiguous -->
