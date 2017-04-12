692159f575e54dd3b334986314f239d8daf8c325
Larry Kagan <larry@superiocity.com>
2017-04-12 18:15:08 -0400

    Updated footer.

--- footer.php
+++ footer.php
@@ -1,31 +1,35 @@
 			</div> <!-- .main -->
 			<footer class="site-footer">
 				<div class="arrow-down light"></div>
-                <div class="columns">
-					<div class="column1">
-						<address>
-							Superiocity Web Development &amp; Design<br />
-							3516 Hudson Lane<br />
-							Boynton Beach, FL 33436<br />
-						</address>
-						<div class="phone"><i class="fa fa-phone"></i> <?php if( function_exists( 'eeb_content' ) )  echo eeb_content('<a href="tel:5613361119">(561) 336-1119</a>'); ?></div>
-						<div class="email"><i class="fa fa-envelope"></i> <span class="emailLink"><?php if( function_exists( 'eeb_content' ) ) echo eeb_content('<a href="mailto:service@superiocity.com">service@superiocity.com</a>'); ?></span></div>
-					</div>
-					<div class="column2">
-						Office Hours: <br />
-						Monday - Friday<br />
-						9 am - 5 pm Eastern
-					</div>
-                </div>
-                <div class="nav">
-                    <a href="/privacy-policy/" title="privacy policy">privacy</a>
-                    <span class="seperator">|</span>
-                    <a href="/terms-of-service/" title="terms of service">terms</a>
-                    <span class="seperator">|</span>
-                    <a href="/sitemap/" title="sitemap">sitemap</a>
-                </div>
+                <div class="centered-block">
+					<div class="columns">
+						<div class="column1">
+                            <h4>Superiocity Web Development &amp; Design</h4>
+							<div class="phone"><i class="fa fa-phone"></i> <?php if( function_exists( 'eeb_content' ) )  echo eeb_content('<a href="tel:5613361119">(561) 336-1119</a>'); ?></div>
+							<div class="email"><i class="fa fa-envelope"></i> <span class="emailLink"><?php if( function_exists( 'eeb_content' ) ) echo eeb_content('<a href="mailto:service@superiocity.com">service@superiocity.com</a>'); ?></span></div>
+                            <i class="fa fa-map-marker"></i>
+                            <address>
+                                3516 Hudson Lane<br />
+                                Boynton Beach, FL 33436<br />
+                            </address>
+						</div><!-- .column1 -->
+						<div class="column2">
+                            <h4>Office Hours</h4>
+							Monday - Friday<br />
+							9 am - 5 pm Eastern
+						</div><!-- .column2 -->
+					</div><!-- .columns -->
+                </div><!-- .centered-block -->
                 <div class="copyright">
-                    &copy; 2007-<?php echo date('Y'); ?> Superiocity, Inc.
+                    <span class="links">
+						<span class="item"><a href="/privacy-policy/" title="privacy policy">privacy</a></span>
+						<span class="seperator">|</span>
+						<span class="item"><a href="/terms-of-service/" title="terms of service">terms</a></span>
+						<span class="seperator">|</span>
+						<span class="item"><a href="/sitemap/" title="sitemap">sitemap</a></span>
+                    </span><!-- .links -->
+					<span class="seperator">|</span>
+					<span class="item">&copy; 2007-<?php echo date('Y'); ?> Superiocity, Inc.</span>
                 </div>
 			</footer>
 		</div> <!-- .page -->
