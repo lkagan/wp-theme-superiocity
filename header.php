4697ffbc849ad81a6cbd00b151ac81ef83079bd6
Larry Kagan <larry@superiocity.com>
2017-04-12 15:07:39 -0400

    changes brought in from production.

--- header.php
+++ header.php
@@ -41,36 +41,38 @@
 
 <body <?php body_class( is_front_page() ? 'home' : 'inner' ); ?>>
     <div class="page">
-        <header class="site-header">
-            <div class="branding">
-                <div class="first-line">
-					<a href="/"><img src="<?= bloginfo('template_url'); ?>/images/superiocity.svg" width="120" class="logo" alt="Superiocity Web Development and Design"></a>
-					<div class="comp-name">
-						<a href="/">
-							<?php if ( is_front_page() ): ?>
-								<h1 class="company-name">Superiocity</h1>
-							<?php else: ?>
-								<h2 class="company-name">Superiocity</h2>
-							<?php endif; ?><span class="trademark">&reg;</span>
-						</a>
+			<header class="site-header">
+				<div class="branding">
+					<div class="first-line">
+						<a href="/"><img src="<?= bloginfo('template_url'); ?>/images/superiocity.svg" width="120" class="logo" alt="Superiocity Web Development and Design"></a>
+						<div class="comp-name">
+							<a href="/">
+								<?php if ( is_front_page() ): ?>
+									<h1 class="company-name">Superiocity</h1>
+								<?php else: ?>
+									<h2 class="company-name">Superiocity</h2>
+								<?php endif; ?><span class="trademark">&reg;</span>
+							</a>
+						</div>
 					</div>
-                </div>
-                <h2><?php bloginfo( 'description' ) ?></h2>
-            </div>
-	        <?php if( has_nav_menu( 'superiocity-main' ) ): ?>
-            <div class="menu-button menu-open">
-                <a href="#" id="menu-button"><i class="fa fa-bars"></i> menu</a>
-            </div>
-            <div class="main-nav-wrapper">
-                <a href="#" class="menu-close">close <i class="fa fa-angle-right"></i> </a>
-				<?php wp_nav_menu( array( 'container' => 'nav' )); ?>
-                <div class="menu-contact">
-                    <span class="phone"><i class="fa fa-phone"></i> <?php if( function_exists( 'eeb_content' ) )  echo eeb_content('<a href="tel:5613361119">(561) 336-1119 <div class="tip"><i class="fa fa-hand-pointer-o"></i> tap to call</div></a>'); ?></span><br>
-                    <span class="email"><i class="fa fa-envelope"></i> <span class="emailLink"><?php if( function_exists( 'eeb_content' ) ) echo eeb_content('<a href="mailto:larry@superiocity.com">larry@superiocity.com</a>'); ?></span></span>
-                </div>
-            </div>
-        </header>
+					<h2><?php bloginfo( 'description' ) ?></h2>
+				</div>
+				<?php if( has_nav_menu( 'superiocity-main' ) ): ?>
+				<div class="menu-button menu-open">
+					<a href="#" id="menu-button"><i class="fa fa-bars"></i> menu</a>
+				</div>
+				<div class="main-nav-wrapper">
+					<a href="#" class="menu-close">close <i class="fa fa-angle-right"></i> </a>
+					<?php wp_nav_menu( array( 'container' => 'nav' )); ?>
+					<div class="menu-contact">
+						<span class="phone"><i class="fa fa-phone"></i> <?php if( function_exists( 'eeb_content' ) )  echo eeb_content('<a href="tel:5613361119">(561) 336-1119 <div class="tip"><i class="fa fa-hand-pointer-o"></i> tap to call</div></a>'); ?></span><br>
+						<span class="email"><i class="fa fa-envelope"></i> <span class="emailLink"><?php if( function_exists( 'eeb_content' ) ) echo eeb_content('<a href="mailto:service@superiocity.com">service@superiocity.com</a>'); ?></span></span>
+					</div>
+				</div>
+				<?php endif; ?>
+			</header>
             <div class="masthead">
+                <div class="background-overlay"></div>
                 <div class="video-bg">
                     <video id="bubble-video" src="<?php echo get_template_directory_uri() ?>/bubbles.webm" autoplay loop poster="<?php echo get_template_directory_uri() ?>/images/bubbles-poster.jpg">
                         <source src="<?php echo get_template_directory_uri() ?>/bubbles.webm">
@@ -94,5 +96,4 @@
                 </div><!-- .wrapper -->
                 <div class="arrow-down dark"></div>
             </div><!-- .masthead -->
-			<?php endif; ?>
         <div class="main clearfix" >
